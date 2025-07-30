f5dbe0929b0cf52defabe908e154acc5c30a3aab
Lee Moon hee <ways0210@qq.com>
2025-07-30 15:29:22 +0800

    Update AwemeHeaders.h

--- AwemeHeaders.h
+++ AwemeHeaders.h
@@ -1116,6 +1116,9 @@ static CGFloat gStartVal = 0.0;
 @interface AWEFullPageFeedNewContainerViewController : UIViewController
 - (BOOL)prefersStatusBarHidden;
 @end
+@interface AFDPureModePageContainerViewController : UIViewController
+- (BOOL)prefersStatusBarHidden;
+@end
 
 @interface AWEFeedUnfollowFamiliarFollowAndDislikeView : UIView
 @end
